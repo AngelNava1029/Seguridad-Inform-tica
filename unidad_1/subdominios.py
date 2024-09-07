ff996226e077604e0c9f717cc7f241a547eec53a
AngelNava1029 <a2231116@uthermosillo.edu.mx>
2024-09-06 19:29:56 -0700

    subdominios

---
+++ unidad_1/subdominios.py
@@ -0,0 +1,10 @@
+import requests
+from os import path
+import argparse
+import sys
+
+parse = argparse.ArgumentParser()
+print(f'{parse}\n\n')
+parse.add_argument('-t','--target',help='indica el dominio de la vistima')
+parse=parse.parse_args()
+print(parse)
